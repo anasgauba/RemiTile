4b50a7e49528fbac724845c595d5d63cd9b086bb
Anas Gauba <anas.farooq71@gmail.com>
2018-09-07 13:41:38 -0600

    Hand class for players

--- src/Player.java
+++ src/Player.java
@@ -1,7 +1,24 @@
+import java.util.LinkedList;
+
 /**
  * @version date: 2018-09-04
  * @author Anas Farooq Gauba
  */
-public interface Player {
-    public void checkForWin();
+public class HumanPlayer implements Player{
+
+//    private Hand myHand;
+//    TilePool tilePool;
+    TileCards hand;
+    TileCards discardPile;
+
+    public HumanPlayer(TileCards discardPile) {
+        this.discardPile = discardPile;
+        this.hand = new TileCards();
+
+    }
+
+    public void play() {
+
+    }
+
 }
