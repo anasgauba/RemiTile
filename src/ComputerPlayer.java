6bfff6c2a3482651e10792f4ee8b9529d0fdba93
Anas Gauba <anas.farooq71@gmail.com>
2018-09-11 20:05:49 -0600

    Added little bit to computer player, need to revisit

--- src/ComputerPlayer.java
+++ src/ComputerPlayer.java
@@ -3,11 +3,16 @@
  * @author Anas Farooq Gauba
  */
 public class ComputerPlayer extends Player {
+    TilePool tile;
 
     public ComputerPlayer(GameCoordinator gameCoordinator) {
         super(gameCoordinator);
     }
 
 
+    public void makeMove() {
+        this.addToHand(tile.getTile());
+        this.discard();
+    }
 
 }
